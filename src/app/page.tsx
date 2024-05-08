77328d5ff23f81f99fd57eae4e4b063f6a3d6596
hayato94087 <hayato94087@gmail.com>
2024-05-08 22:29:30 +0900

    feat: lucide reactを利用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 import { type FC } from "react";
+import { Camera } from "lucide-react";
 
 const Home: FC = () => {
   return (
-    <div className="">
-      <div className="text-lg font-bold">Home</div>
-      <div>
-        <span className="text-blue-500">Hello</span>
-        <span className="text-red-500">World</span>
-      </div>
+    <div className="p-3 flex flex-row space-x-2">
+      {/* 線の色は赤色 */}
+      <Camera color="red" />
+      {/* 線の色は青色 */}
+      <Camera color="blue" />
+      {/* 線の色は赤色, サイズは 24px x 24px */}
+      <Camera color="red" size={24} />
+      {/* 線の色は赤色, サイズは 24px x 24px, 線の太さは1px */}
+      <Camera color="red" size={24} strokeWidth={1} />
+      {/* 線の色は赤色, サイズは 24px x 24px, 線の太さは2px */}
+      <Camera color="red" size={24} strokeWidth={2} />
+      {/* 線の色は緑色, サイズは 24px x 24px, 線の太さは2px,  */}
+      <Camera size={24} strokeWidth={2} className="text-green-500" />
+      {/* 線の色は黒色, サイズは 24px x 24px, 線の太さは2px, 背景色は赤 */}
+      <Camera size={24} strokeWidth={2} className="bg-red-200" />
+      {/* 線の色は黒色, サイズは 24px x 24px, 線の太さは2px, アニメーション */}
+      <Camera size={24} strokeWidth={2} className="animate-spin" />
+      {/* サイズは 40px x 40px, 線の太さは2px */}
+      <Camera strokeWidth={2} className="w-10 h-10" />
+      {/* 線の太さは2px, ホバーした際に線の色を黒から赤色に変更 */}
+      <Camera strokeWidth={2} className="hover:text-red-500" />
     </div>
   );
 };
